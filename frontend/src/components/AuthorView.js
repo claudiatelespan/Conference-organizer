2cc4f3bc89eaf4dcc22e6f2e239b77810de9636a
Claudia Telespan <claudiatelespan@gmail.com>
2025-01-21 00:04:46 +0200

    rezolvat cu formul pentru upload si update article

--- frontend/src/components/AuthorView.js
+++ frontend/src/components/AuthorView.js
@@ -11,18 +11,35 @@ const AuthorView = ({
   onUploadArticle 
 }) => {
   
+  const [showUploadModal, setShowUploadModal] = useState(false);
+
   const handleFileUpload = async (event, existingArticleId = null) => {
-    const file = event.target.files[0];
-    if (!file) return;
-  
-    const articleData = {
-      title: file.name,
-    };
+    let title, file, articleData;
+
+    if (existingArticleId === null){
+      event.preventDefault();
+      title = event.target[0].value;
+      file = event.target[1].files[0];
+      if (!file || !title) return;
+    
+      articleData = {
+        title: title,
+      };
+    }
+    else {
+      file = event.target.files[0];
+      if (!file) return;
+    
+      articleData = {
+        title: file.name,
+      };
+    }
   
     try {
       const updatedArticle = await uploadArticle(selectedConference.id, articleData, file, existingArticleId);
       console.log("Articol actualizat cu succes:", updatedArticle);
-      onUploadArticle(selectedConference.id, articleData, existingArticleId); 
+      onUploadArticle(selectedConference.id, articleData, existingArticleId);
+      setShowUploadModal(false);
     } catch (error) {
       console.error("Eroare la încărcarea articolului:", error);
     }
@@ -35,16 +52,37 @@ const AuthorView = ({
       <h2>Conferință: {selectedConference.title}</h2>
         <div className="articles-section">
           <div className="upload-section">
-            <input
-              type="file"
-              id="article-upload"
-              onChange={(e) => handleFileUpload(e)}
-              accept=".pdf,.doc,.docx"
-              style={{ display: 'none' }}
-            />
-            <label htmlFor="article-upload" className="upload-button">
+            <button htmlFor="article-upload" className="upload-button" onClick={() => setShowUploadModal(true)}>
               Încarcă articol nou
-            </label>
+            </button>
+            {showUploadModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={() => setShowUploadModal(false)}>&times;</span>
+            <h2>Încarcă articol</h2>
+            <form onSubmit={handleFileUpload}>
+              <div className='form-group'>
+                <label>Titlu</label>
+                <input
+                  type="text"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label>Fișier</label>
+                <input
+                  type="file"
+                  accept=".pdf,.doc,.docx"
+                  required
+                />
+              </div>
+              <div className='form-actions'>
+                <button type="submit" className='download'>Încarcă</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
           </div>
 
           <div className="articles-list">
@@ -57,9 +95,9 @@ const AuthorView = ({
                   >
                     Descarcă articol
                 </button>
-                <label htmlFor={`update-${article.id}`} className="upload-button">
+                  <label htmlFor={`update-${article.id}`} className="upload-button">
                     Încarcă versiune nouă
-                </label>
+                  </label>
                 <p>
                   <strong>Status:</strong>{' '}
                   <span className={`status ${article.status ==='accepted' ? 'approved' : 'pending'}`}>
@@ -82,13 +120,13 @@ const AuthorView = ({
                 </div>
 
                 <div className="article-actions">
-                  <input
-                    type="file"
-                    id={`update-${article.id}`}
-                    onChange={(e) => handleFileUpload(e, article.id)}
-                    accept=".pdf,.doc,.docx"
-                    style={{ display: 'none' }}
-                  />
+                <input
+                  type="file"
+                  id={`update-${article.id}`}
+                  onChange={(e) => handleFileUpload(e, article.id)}
+                  accept=".pdf,.doc,.docx"
+                  style={{ display: 'none' }}
+                />
                 </div>
               </div>
             ))}
